395d7b3104d97b844808ce7485612d3b3922e8e0
Matt Dawson <mattdawson18@gmail.com>
2021-07-27 11:06:17 +0100

    Basic signup page that can access text input

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 Builder.load_file('design.kv')
 
 class LoginScreen(Screen):
-    pass
+    def sign_up(self):
+        self.manager.current = "Sign_up_screen"
+
+class SignUpScreen(Screen):
+    def add_user(self, uname, pword):
+        print(uname, pword)
 
 
 class RootWidget(ScreenManager):
